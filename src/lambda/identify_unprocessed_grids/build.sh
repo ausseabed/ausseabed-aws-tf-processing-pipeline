4b446cd84f96e85a9aea960f3d64a596bf666850
Callum McKenna <callum.mckenna@ga.gov.au>
2022-03-30 19:36:08 +1100

    Update Lambda build script

--- src/lambda/identify_unprocessed_grids/build.sh
+++ src/lambda/identify_unprocessed_grids/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -t . pip==21.1.3
-./bin/pip install -t . msal==1.6.0 python-json-logger==2.0.1 requests==2.25.0
+python -m pip install --upgrade -t . msal==1.6.0 python-json-logger==2.0.1 requests==2.25.0
 curl -L https://github.com/ausseabed/product-catalogue/archive/master.zip -o master.zip
 unzip master.zip 
 mv product-catalogue-master/py-rest-client/product_catalogue_py_rest_client .
