9876100aeeec74d63e277df8be9e34543d38b2d2
Callum McKenna <callum.mckenna@ga.gov.au>
2022-03-30 19:20:03 +1100

    Update build to set dependency versions

--- src/lambda/identify_unprocessed_grids/build.sh
+++ src/lambda/identify_unprocessed_grids/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-pip install --upgrade pip
-pip install -t . msal python-json-logger requests boto3
+pip install -t . msal==1.6.0 python-json-logger==2.0.1 requests==2.25.0
 curl -L https://github.com/ausseabed/product-catalogue/archive/master.zip -o master.zip
 unzip master.zip 
 mv product-catalogue-master/py-rest-client/product_catalogue_py_rest_client .
